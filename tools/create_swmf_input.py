152a5805c6bc5fc4d1bd56359871c697652a54a2
Yuxi Chen <yuxichen@umich.edu>
2026-08-11 11:52:48 -0400

    docs: add SWMF CLI example

--- tools/create_swmf_input.py
+++ tools/create_swmf_input.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
-"""Generate an SWMF input file from interval-averaged MMS observations."""
+"""Generate an SWMF input file from interval-averaged MMS observations.
+
+Examples
+--------
+Generate the SWMF input and save the MMS quick-look plot::
+
+    ./tools/create_swmf_input.py \
+        --mms-start "2018-12-19 19:40:00" \
+        --mms-end "2018-12-19 19:52:00" \
+        --plot
+
+The plot is saved as
+``mms_20181219_194000_20181219_195200.png`` beside the SWMF input.
+"""
 
 from shocklink.mms_swmf import main
 
